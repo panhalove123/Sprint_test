a14f62aa75cadc3fc583fcd49150e7598dc2aaf1
Lethean2005 <lethean.seourn@student.passerellesnumeriques.org>
2025-03-03 08:39:47 +0700

    feat(manage-user:create table-user)

--- Router/route.php
+++ Router/route.php
@@ -3,9 +3,12 @@ require_once "Router.php";
 require_once "Controllers/BaseController.php";
 require_once "Database/Database.php";
 require_once "Controllers/WelcomeController.php";
+require_once "Controllers/UserController.php";
 
 
 $route = new Router();
 $route->get("/", [WelcomeController::class, 'welcome']);
+$route->get("/manage-user", [UserController::class, 'manageUser']);
+
 
 $route->route();
